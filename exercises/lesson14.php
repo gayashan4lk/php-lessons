bbe1d056314b53bd3d76a167c1253baf4df8dc25
Gayashan Wagachchi <gayashan4lk@gmail.com>
2014-06-06 15:59:01 -0400

    lesson 14

---
+++ exercises/lesson14.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>test</title>
+</head>
+<body>
+<?php  
+	$a = 1;
+?>
+<?php if($a == 1){ ?>
+<p>a is equal to 1</p> 
+<?php } else { ?>
+<p>a is other one.not 1! </p> 
+<?php  } ?> 
+</body>
+</html>

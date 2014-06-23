58e60d316373fe1ce70f8e45f0bba3918cae84e2
Gayashan Wagachchi <gayashan4lk@gmail.com>
2014-06-22 22:20:01 -0400

    murach php

---
+++ ex-files-murach-php-book/ex_starts/ch02_ex1/display_discount.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title>Product Discount Calculator</title>
+    <link rel="stylesheet" type="text/css" href="main.css" />
+</head>
+<body>
+    <div id="content">
+        <h1>This page is under construction</h1>
+
+        <label>Product Description:</label>
+        <span><?php echo $product_description; ?></span><br />
+
+        <label>List Price:</label>
+        <span><?php echo $list_price_formatted; ?></span><br />
+
+        <label>Standard Discount:</label>
+        <span><?php echo $discount_percent_formatted; ?></span><br />
+
+        <label>Discount Amount:</label>
+        <span><?php echo $discount_formatted; ?></span><br />
+
+        <label>Discount Price:</label>
+        <span><?php echo $discount_price_formatted; ?></span><br />
+
+        <p>&nbsp;</p>
+    </div>
+</body>
+</html>
